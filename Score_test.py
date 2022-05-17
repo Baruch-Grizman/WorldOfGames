fc000a821bfe96ab9ebfd70daaec5c61555917b7
Baruch-Grizman <103598882+Baruch-Grizman@users.noreply.github.com>
2022-05-17 15:50:01 +0300

    adding score function

--- Score_test.py
+++ Score_test.py
@@ -3,7 +3,7 @@ A package that is in charge of managing the scores file.
 The scores file at this point will consist of only a number. That number is the accumulation of the
 winnings of the user. Amount of points for winning a game is as follows:
 POINTS_OF_WINNING = (DIFFICULTY X 3) + 5
-Each time the user is winning a game, the points he one will be added to his current amount of
+Each time the user is winning a game, the points he won will be added to his current amount of
 point saved in a file.
 Methods
     1.  add_score - The function’s input is a variable called difficulty. The function will try to read
@@ -12,11 +12,31 @@ Methods
 """
 
 
-from Live_test import *
+# from Live_test import *
+
 
 class Score:
 
-    def add_score(Live.GAME_DIFF):
-        POINTS_OF_WINNING = (DIFFICULTY X 3) + 5
+    def __init__(self):
+        # self.SCORE_DIFFICULTY = Live.DIFFICULTY
+        self.SCORE_DIFFICULTY = 1
+
+    def add_score(self):
+        points_of_winning = (self.SCORE_DIFFICULTY*3)+5
+        try:
+            with open('Scores.txt', encoding='utf-8') as score_file:
+                for line in score_file.readlines():
+                    points_of_winning += int(line)
+            with open('Scores.txt', 'w+', encoding='utf-8') as score_file:
+                score_file.write(f'{points_of_winning}')
+
+        except:
+            with open('Scores.txt', 'w+', encoding='utf-8') as score_file:
+                score_file.write(f'{points_of_winning}')
+
+
+Score = Score()
+print(Score.add_score())
+
 
 
